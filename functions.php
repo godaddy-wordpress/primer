fd97599fba86a95e1966dc4ef6158e399a114f41
Chris Wallace <chris@liftux.com>
2014-12-30 08:54:02 -0600

    Moved javascript files to different folder

--- functions.php
+++ functions.php
@@ -208,9 +208,9 @@ function basis_scripts() {
 
 	wp_style_add_data( 'basis', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'basis-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	wp_enqueue_script( 'basis-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20120206', true );
 
-	wp_enqueue_script( 'basis-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'basis-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
