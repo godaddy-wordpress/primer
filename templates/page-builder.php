43e8592f2656409a651cc11ca3eec85897e5d61f
Chris Wallace <chris@liftux.com>
2015-03-23 23:15:33 -0500

    Added page builder template

---
+++ templates/page-builder.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Template Name: Page Builder
+*/
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php the_content(); ?>
+
+			<?php endwhile; ?>
+
+			<?php basis_paging_nav(); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
