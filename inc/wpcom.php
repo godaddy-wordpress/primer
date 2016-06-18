d0274ff3135fb91efbce9e6a58e4f67b51e22dac
Frankie Jarrett <fjarrett@godaddy.com>
2016-06-17 20:06:03 -0500

    Lots of code formatting

--- inc/wpcom.php
+++ inc/wpcom.php
@@ -10,20 +10,27 @@
 /**
  * Adds support for wp.com-specific theme functions.
  *
+ * @action after_setup_theme
+ *
  * @global array $themecolors
  */
 function primer_wpcom_setup() {
+
 	global $themecolors;
 
-	// Set theme colors for third party services.
-	if ( ! isset( $themecolors ) ) {
-		$themecolors = array(
-			'bg'     => '',
-			'border' => '',
-			'text'   => '',
-			'link'   => '',
-			'url'    => '',
-		);
+	if ( isset( $themecolors ) ) {
+
+		return;
+
 	}
+
+	$themecolors = array(
+		'bg'     => '',
+		'border' => '',
+		'text'   => '',
+		'link'   => '',
+		'url'    => '',
+	);
+
 }
 add_action( 'after_setup_theme', 'primer_wpcom_setup' );
