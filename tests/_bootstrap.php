2cad9d4ecb393902accce347dc811bc25cbd2582
Frankie Jarrett <fjarrett@godaddy.com>
2016-06-23 11:16:07 -0500

    Move bootstrap

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,2 +1,18 @@
 <?php
 // This is global bootstrap for autoloading
+
+// So we know this is only a test
+define( 'DOING_TESTS', true );
+
+// Find the ABSPATH
+$abspath = explode( 'wp-content', getcwd() );
+
+// Load WordPress
+require_once $abspath[0] . 'wp-load.php';
+
+// Switch theme
+switch_theme( 'primer' );
+
+// Make sure these are installed for our tests
+exec( 'wp core language install es_ES' );
+exec( 'wp plugin install monster-widget --activate' );
--- tests/acceptance/_bootstrap.php
+++ tests/acceptance/_bootstrap.php
@@ -1,18 +1,2 @@
 <?php
 // Here you can initialize variables that will be available to your tests
-
-// So we know this is only a test
-define( 'DOING_TESTS', true );
-
-// Find the ABSPATH
-$abspath = explode( 'wp-content', getcwd() );
-
-// Load WordPress
-require_once $abspath[0] . 'wp-load.php';
-
-// Switch theme
-switch_theme( 'primer' );
-
-// Make sure these are installed for our tests
-exec( 'wp core language install es_ES' );
-exec( 'wp plugin install monster-widget --activate' );
