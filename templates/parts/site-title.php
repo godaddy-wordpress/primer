ca71d84ab12ac7c9ca0126a53dc4e30d8e62fc13
Toby Schrapel <toby@tobyschrapel.com>
2016-07-04 00:01:09 +0100

    Fix WPCS

--- templates/parts/site-title.php
+++ templates/parts/site-title.php
@@ -10,11 +10,11 @@
 
 	<div class="site-title-wrapper-inner">
 
-		<?php if( has_custom_logo() ): ?>
+		<?php if ( has_custom_logo() ) : ?>
 
 			<?php the_custom_logo(); ?>
 
-		<?php else: ?>
+		<?php else : ?>
 
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<div class="site-description"><?php bloginfo( 'description' ); ?></div>
