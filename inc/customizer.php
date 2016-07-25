7bae8a723ac4a87d8c648d03e7907f0a6f884cb9
Frankie Jarrett <fjarrett@godaddy.com>
2016-07-25 15:53:12 -0500

    Remove postMessage line, #1

--- inc/customizer.php
+++ inc/customizer.php
@@ -72,7 +72,6 @@ class Primer_Customizer {
 		$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 		$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 		$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
-		$wp_customize->get_setting( 'background_color' )->transport = 'postMessage';
 
 		if ( ! isset( $wp_customize->selective_refresh ) ) {
 
