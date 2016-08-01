56a99e0fb08ce3a2366499f301553120dcde6476
Frankie Jarrett <fjarrett@gmail.com>
2016-08-01 11:40:56 -0500

    Revert "Revert "Susy refactor""

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -16,6 +16,7 @@ window.onload = function() {
 
 	menu_toggle.onclick = function() {
 
+		menu_toggle.classList.toggle('open');
 		nav_menu.style.display = ( ! nav_menu.offsetHeight ) ? 'block' : 'none';
 
 	};
