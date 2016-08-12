925ded1d760f223039e0bd0b4569a4e5a14de0ed
Frankie Jarrett <fjarrett@godaddy.com>
2016-08-12 01:33:41 -0500

    Make theme link in site-info more robust

--- templates/parts/site-info.php
+++ templates/parts/site-info.php
@@ -25,14 +25,12 @@
 
 				echo ' &mdash; ';
 
-				$theme_name = get_option( 'current_theme' );
-
 				printf(
 					esc_html_x( '%1$s theme by %2$s', '1. theme name link, 2. theme author link', 'primer' ),
 					sprintf(
 						'<a href="https://wordpress.org/themes/%s/" rel="designer">%s</a>',
-						sanitize_key( $theme_name ),
-						esc_html( $theme_name )
+						sanitize_key( get_stylesheet() ),
+						esc_html( get_option( 'current_theme', ucwords( get_stylesheet() ) ) )
 					),
 					'<a href="https://www.godaddy.com/" rel="designer">GoDaddy</a>'
 				);
