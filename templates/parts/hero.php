33ef6875c094fc3e2c479445b3c7c4b2a592d783
Frankie Jarrett <fjarrett@godaddy.com>
2016-08-15 11:16:09 -0500

    Add hero inner element

--- templates/parts/hero.php
+++ templates/parts/hero.php
@@ -17,13 +17,17 @@ $hero_style = (string) apply_filters( 'primer_hero_style_attr', '' );
 
 <div class="hero"<?php if ( $hero_style ) : ?> style="<?php echo $hero_style; // xss ok ?>"<?php endif; ?>>
 
-	<?php
-	/**
-	 * Fires inside the `.hero` element.
-	 *
-	 * @since 1.0.0
-	 */
-	do_action( 'primer_hero' );
-	?>
+	<div class="hero-inner">
+
+		<?php
+		/**
+		 * Fires inside the `.hero` element.
+		 *
+		 * @since 1.0.0
+		 */
+		do_action( 'primer_hero' );
+		?>
+
+	</div>
 
 </div>
