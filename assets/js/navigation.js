60b066a9ee703a0d47150ced0c939c9b9de0be92
Jonathan Bardo <bardo.jonathan@gmail.com>
2016-08-17 22:07:05 -0400

    Small improvement. Using class instead of inline css to toggle menu open

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -9,8 +9,7 @@
 	$( document ).ready( function() {
 
 		var $nav_menu    = $( '#site-navigation' ),
-				$menu_toggle = $( '#menu-toggle' ),
-				open         = false;
+				$menu_toggle = $( '#menu-toggle' );
 
 		if ( ! $nav_menu || ! $menu_toggle ) {
 
@@ -20,40 +19,32 @@
 
 		$menu_toggle.click( function() {
 
-			open = ! open;
-
-			$menu_toggle.toggleClass( 'open' );
-
-			$nav_menu.css( 'display', ( open ) ? 'block' : 'none' );
+			$menu_toggle.add( $nav_menu ).toggleClass( 'open' );
 
 		});
 
-		$nav_menu.find( '.menu-item-has-children' ).hover( function() {
+		$nav_menu.find( '.menu-item-has-children' ).on( 'hover', menuPosition );
+
+		function menuPosition() {
 
 			var $this              = $( this ),
-					$submenu           = $this.children( '.sub-menu' ).eq( 0 ),
-					submenu_position   = $submenu.offset(),
-					submenu_width      = $submenu.width(),
-					window_width       = $( window ).width(),
-					$parent_menu_items = $this.parents( 'ul' ).length,
-					$how_far_back      = 0;
+			    $submenu           = $this.children( '.sub-menu' ).first(),
+			    submenu_position   = $submenu.offset().left,
+			    submenu_width      = $submenu.width();
 
-			if ( ( submenu_position.left + submenu_width ) > window_width || $submenu.parents( '.bump' ).length ) {
 
-				if ( $parent_menu_items > 1 ) {
-
-					$how_far_back = submenu_width;
-
-				}
+			if ( ( submenu_position + submenu_width ) > $( window ).width() || $submenu.parents( '.bump' ).length ) {
 
 				$submenu.addClass( 'bump' ).css({
 					'left'  : 'auto',
-					'right' : $how_far_back
+					'right' : ( $this.parents( 'ul' ).length > 1 ) ? submenu_width : 0
 				});
 
 			}
 
-		});
+			$this.off( 'hover', menuPosition );
+
+		}
 
 	});
 
