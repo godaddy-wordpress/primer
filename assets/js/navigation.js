3318383f43ba0de6a7e592b139e314dfedcef974
Jonathan Bardo <bardo.jonathan@gmail.com>
2016-08-17 22:22:15 -0400

    Compatiment logic into functions

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -6,10 +6,47 @@
 
 ( function( $ ) {
 
+	var $nav_menu    = false,
+	    $menu_toggle = false;
+
+	function toggle() {
+
+		$menu_toggle.add( $nav_menu ).toggleClass( 'open' );
+
+	}
+
+	function position() {
+
+		var $this    = $( this ),
+		    $submenu = $this.children( '.sub-menu' ).first();
+
+
+		if ( isOffScreen( $submenu ) || $submenu.parents( '.bump' ).length ) {
+
+			$submenu.addClass( 'bump' ).css({
+				'left'  : 'auto',
+				'right' : ( $this.parents( 'ul' ).length > 1 ) ? $submenu.width() : 0
+			});
+
+		}
+
+		$this.off( 'hover', position );
+
+	}
+
+	function isOffScreen( $submenu ) {
+
+		var submenu_position   = $submenu.offset().left,
+		    submenu_width      = $submenu.width();
+
+		return ( submenu_position + submenu_width ) > $( window ).width();
+
+	}
+
 	$( document ).ready( function() {
 
-		var $nav_menu    = $( '#site-navigation' ),
-		    $menu_toggle = $( '#menu-toggle' );
+		$nav_menu    = $( '#site-navigation' );
+		$menu_toggle = $( '#menu-toggle' );
 
 		if ( ! $nav_menu || ! $menu_toggle ) {
 
@@ -17,34 +54,9 @@
 
 		}
 
-		$menu_toggle.click( function() {
+		$menu_toggle.on( 'click', toggle );
 
-			$menu_toggle.add( $nav_menu ).toggleClass( 'open' );
-
-		});
-
-		$nav_menu.find( '.menu-item-has-children' ).on( 'hover', menuPosition );
-
-		function menuPosition() {
-
-			var $this              = $( this ),
-			    $submenu           = $this.children( '.sub-menu' ).first(),
-			    submenu_position   = $submenu.offset().left,
-			    submenu_width      = $submenu.width();
-
-
-			if ( ( submenu_position + submenu_width ) > $( window ).width() || $submenu.parents( '.bump' ).length ) {
-
-				$submenu.addClass( 'bump' ).css({
-					'left'  : 'auto',
-					'right' : ( $this.parents( 'ul' ).length > 1 ) ? submenu_width : 0
-				});
-
-			}
-
-			$this.off( 'hover', menuPosition );
-
-		}
+		$nav_menu.find( '.menu-item-has-children' ).on( 'hover', position );
 
 	});
 
