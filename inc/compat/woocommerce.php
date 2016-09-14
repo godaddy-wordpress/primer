b78763318fce4a73194fb0b545ba4348ac000e56
Evan Herman <evan.m.herman@gmail.com>
2016-09-14 14:20:13 -0400

    Remove the 'layout' options from the customizer, and fix WooCommerce shop page layout.

--- inc/compat/woocommerce.php
+++ inc/compat/woocommerce.php
@@ -158,7 +158,7 @@ add_filter( 'primer_the_page_title', 'primer_wc_shop_title' );
  */
 function primer_wc_shop_columns( $columns ) {
 
-	if ( is_shop() && 0 === strpos( primer_get_layout(), 'three-column-' ) ) {
+	if ( is_shop() && 0 === strpos( primer_get_layout( wc_get_page_id( 'shop' ) ), 'three-column-' ) ) {
 
 		add_filter( 'post_class', 'primer_wc_product_classes' );
 
