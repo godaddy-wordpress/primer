5b2cd3cf5e43d2cdce2620c91a271a8fcf16d90f
Evan Herman <evan.m.herman@gmail.com>
2016-12-12 18:44:49 -0500

    Start to build in support for video headers.

--- inc/customizer/colors.php
+++ inc/customizer/colors.php
@@ -1146,6 +1146,7 @@ class Primer_Customizer_Colors {
 				'height'             => 1300,
 				'flex-height'        => true,
 				'wp-head-callback'   => array( $this, 'header_css' ),
+				'video'              => true,
 			)
 		);
 
