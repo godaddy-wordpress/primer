e9512c279eac0aa74f9bc707720bab8740104819
Evan Herman <evan.m.herman@gmail.com>
2017-01-25 17:02:22 -0500

    Initial docs test

--- templates/parts/loop/page-footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Template part for displaying the page footer inside The Loop.
- *
- * @package Primer
- * @since   1.0.0
- */
-
-edit_post_link( esc_html__( 'Edit', 'primer' ), '<span class="edit-link">', '</span>' );
