3ab6a7d74f5c77354730e04dc7b66452ebfc701b
Evan Herman <evan.m.herman@gmail.com>
2017-03-28 13:50:21 -0400

    Fix spacing

--- templates/parts/loop/post-excerpt.php
+++ templates/parts/loop/post-excerpt.php
@@ -12,8 +12,6 @@
 
 	<?php the_excerpt(); ?>
 
-	<p>
-		<a class="button" href="<?php the_permalink(); ?>" aria-label="<?php /* translators: %s: Post title */ printf( esc_attr__( 'Continue reading %s', 'primer' ), get_the_title() ); ?>"><?php /* translators: %s: Left/Right arrow */ printf( esc_html__( 'Continue Reading %s', 'primer' ), is_rtl() ? '&larr;' : '&rarr;' ); ?></a>
-	</p>
+	<p><a class="button" href="<?php the_permalink(); ?>" aria-label="<?php /* translators: %s: Post title */ printf( esc_attr__( 'Continue reading %s', 'primer' ), get_the_title() ); ?>"><?php /* translators: %s: Left/Right arrow */ printf( esc_html__( 'Continue Reading %s', 'primer' ), is_rtl() ? '&larr;' : '&rarr;' ); ?></a></p>
 
 </div><!-- .entry-summary -->
