5ef4f12485fb610b90b3c642b5b9d853e21141fc
Evan Herman <evan.m.herman@gmail.com>
2017-03-28 13:45:23 -0400

    Set %1 to %s

--- templates/parts/loop/post-excerpt.php
+++ templates/parts/loop/post-excerpt.php
@@ -13,7 +13,7 @@
 	<?php the_excerpt(); ?>
 
 	<p>
-		<a class="button" href="<?php the_permalink(); ?>" aria-label="<?php /* translators: %s: Post title */ printf( esc_attr__( 'Continue reading %1$s', 'primer' ), get_the_title() ); ?>"><?php /* translators: %s: Left/Right arrow */ printf( esc_html__( 'Continue Reading %s', 'primer' ), is_rtl() ? '&larr;' : '&rarr;' ); ?></a>
+		<a class="button" href="<?php the_permalink(); ?>" aria-label="<?php /* translators: %s: Post title */ printf( esc_attr__( 'Continue reading %s', 'primer' ), get_the_title() ); ?>"><?php /* translators: %s: Left/Right arrow */ printf( esc_html__( 'Continue Reading %s', 'primer' ), is_rtl() ? '&larr;' : '&rarr;' ); ?></a>
 	</p>
 
 </div><!-- .entry-summary -->
