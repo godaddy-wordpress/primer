ac5dabd3816ac069c6257bec958ff6ddc385d92f
Evan Herman <evanmherman@gmail.com>
2017-04-03 14:56:47 -0400

    Continue reading aria labels (#184)
    * Introduce aria-labels onto the 'Continue Reading' links to better communicate intention. Resolves #169

--- templates/parts/loop/post-excerpt.php
+++ templates/parts/loop/post-excerpt.php
@@ -12,6 +12,6 @@
 
 	<?php the_excerpt(); ?>
 
-	<p><a class="button" href="<?php the_permalink(); ?>"><?php printf( /* translators: right arrow (LTR) / left arrow (RTL) */ esc_html__( 'Continue Reading %s', 'primer' ), is_rtl() ? '&larr;' : '&rarr;' ); ?></a></p>
+	<p><a class="button" href="<?php the_permalink(); ?>" aria-label="<?php printf( /* translators: post title */ esc_attr__( 'Continue reading %s', 'primer' ), get_the_title() ); ?>"><?php printf( /* translators: right arrow (LTR) / left arrow (RTL) */ esc_html__( 'Continue Reading %s', 'primer' ), is_rtl() ? '&larr;' : '&rarr;' ); ?></a></p>
 
 </div><!-- .entry-summary -->
