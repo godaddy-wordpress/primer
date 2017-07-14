7686a53f1c73555809146738c318d4f3302ac7f7
Evan Herman <evan.m.herman@gmail.com>
2017-07-14 18:32:52 -0400

    Update

--- .dev/install-wp.sh
+++ .dev/install-wp.sh
@@ -11,7 +11,8 @@ DB_USER=$3
 DB_PASS=$4
 DB_HOST=${5-localhost}
 WP_VERSION=${6-latest}
-TRAVIS_BUILD_DIR=$7
+THEME=$7
+DEV_LIB_PATH=$8
 
 WP_CORE_DIR=${WP_CORE_DIR-/tmp/wordpress/}
 
@@ -106,13 +107,13 @@ install_default_site() {
 
 run_theme_check() {
 
-	cd ${TRAVIS_BUILD_DIR}
+	cd ${DEV_LIB_PATH}
 	grunt build
-	mv -f build ${WP_CORE_DIR}/wp-content/themes/$(basename ${TRAVIS_BUILD_DIR})
+	mv -f build ${WP_CORE_DIR}/wp-content/themes/${THEME}
 	php /tmp/wp-cli.phar package install anhskohbo/wp-cli-themecheck
-	php /tmp/wp-cli.phar theme activate $(basename ${TRAVIS_BUILD_DIR}) --path=${WP_CORE_DIR}
+	php /tmp/wp-cli.phar theme activate ${THEME} --path=${WP_CORE_DIR}
 	php /tmp/wp-cli.phar plugin install theme-check --activate --path=${WP_CORE_DIR}
-	php /tmp/wp-cli.phar themecheck --theme=$(basename ${TRAVIS_BUILD_DIR}) --path=${WP_CORE_DIR}
+	php /tmp/wp-cli.phar themecheck --theme=${THEME} --path=${WP_CORE_DIR}
 
 }
 
