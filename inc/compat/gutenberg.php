0d1e2f177f90e80795b9ba0c73de8b659b5bdc90
Evan Herman <evan.m.herman@gmail.com>
2018-07-10 16:45:13 -0400

    Update gutenberg theme support

--- inc/compat/gutenberg.php
+++ inc/compat/gutenberg.php
@@ -16,20 +16,8 @@
  */
 function primer_gutenberg_theme_support() {
 
-	$theme_support = array(
-		'wide-images' => true,
-	);
-
-	if ( class_exists( 'Primer_Customizer_Colors' ) ) {
-
-		$primer_colors = new Primer_Customizer_Colors();
-		$colors_array  = $primer_colors->get_current_color_scheme_array();
-
-		$theme_support['colors'] = array_values( array_unique( $colors_array['colors'] ) );
-
-	}
-
-	add_theme_support( 'gutenberg', $theme_support );
+	add_theme_support( 'wp-block-styles' );
+	add_theme_support( 'align-wide' );
 
 }
 add_action( 'after_setup_theme', 'primer_gutenberg_theme_support' );
