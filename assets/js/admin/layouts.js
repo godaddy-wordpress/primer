88915a7fd2108d667dc39fd4ffc6e2a29c516501
Evan Herman <evanmherman@gmail.com>
2018-07-16 15:43:56 -0400

    Introduce support for Gutenberg (#240)
    * Introduce support for gutenber image & cover-image blocks.
    
    * Refactor scss, fix typos, remove 1.0.0 for NEXT
    
    * Remove charset. Add missing .alignfull class
    
    * Add docblock for gutenberg theme support function
    
    * Add .alignwide support
    
    * Add primer color scheme colors to Gutenberg them su
    pport
    
    * Tweak layout metabox styles. Add layouts to sass task.
    
    * Update gutenberg block styles
    
    * Tweak pullquote styles, image floats, responsive styles.
    
    * Remove duplicate declaration
    
    * styles for alignfull
    
    * Update gutenberg theme support
    
    * Updates to gutenberg editor width based on page layout
    
    * Tweak gutenberg editor page width CSS and JS
    
    * Resolve phpcs warnings

--- assets/js/admin/layouts.js
+++ assets/js/admin/layouts.js
@@ -28,10 +28,29 @@ window.wp = window.wp || {};
 				.prop( 'disabled', true );
 
 			$( '.primer-layout ul li.global input' )
-				.prop( 'checked', true );
+				.prop( 'checked', true )
+				.trigger( 'change' );
 
+		} );
+
+		$( 'input[name="primer-layout"]' ).change( function() {
+			toggleEditorWidth( $( this ).val() );
 		} );
 
 	} );
 
+	$( document ).on( 'ready', function() {
+		toggleEditorWidth( primerLayouts.selected );
+	} );
+
 } )( jQuery );
+
+function toggleEditorWidth( pageWidth ) {
+
+	pageWidth = pageWidth.indexOf( 'wide' ) >= 0 ? 'wide' : ( pageWidth.indexOf( 'narrow' ) >= 0 || pageWidth.indexOf( 'three-column' ) >= 0 ? 'narrow' : 'default' );
+
+	$( 'body.gutenberg-editor-page' )
+		.removeClass( 'primer-gutenberg-default primer-gutenberg-wide primer-gutenberg-narrow' )
+		.addClass( 'primer-gutenberg-' + pageWidth );
+
+}
--- functions.php
+++ functions.php
@@ -128,6 +128,17 @@ if ( class_exists( 'FLBuilder' ) ) {
 }
 
 /**
+ * Load Gutenberg compatiblity.
+ *
+ * @since NEXT
+ */
+if ( defined( 'GUTENBERG_VERSION' ) ) {
+
+	require_once get_template_directory() . '/inc/compat/gutenberg.php';
+
+}
+
+/**
  * Load Jetpack compatibility file.
  *
  * @since 1.0.0
