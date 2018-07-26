58422efa1e1ec7b77cb13d8204e3dbb24b7e516d
Evan Herman <evanmherman@gmail.com>
2018-07-26 10:26:07 -0400

    Fix nav menu bugs (#263)
    * Fix nav menu bug on initial click
    
    * Tweak JS

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -3,8 +3,8 @@
 ( function( $ ) {
 
 	var $navMenu    = false,
-	    $menuToggle = false,
-	    $submenu    = false;
+			$menuToggle = false,
+			$submenu    = false;
 
 	function toggle() {
 
@@ -88,7 +88,7 @@
 	function isOffScreen( $submenu ) {
 
 		var submenuPosition = $submenu.offset().left,
-		    submenuWidth    = $submenu.width();
+				submenuWidth    = $submenu.width();
 
 		return ( submenuPosition + submenuWidth ) > $( window ).width();
 
@@ -107,11 +107,16 @@
 
 		$menuToggle.on( 'click', toggle );
 
-		$navMenu.find( '.menu-item-has-children' ).on( 'hover', position );
+		$navMenu.find( '.menu-item-has-children' ).on( 'hover, focusin', position );
 
-		$navMenu.find( '.menu-item > a' ).on( 'focusin', tabNavigationMenuIn );
-
-		$navMenu.find( '.menu-item > a' ).on( 'focusout', tabNavigationMenuOut );
+		$navMenu.find( '.menu-item > a' ).on( 'keyup', function( e ) {
+			if ( 9 !== e.which ) {
+				return;
+			}
+			$menuItem = $navMenu.find( '.menu-item > a' );
+			$menuItem.on( 'focusin', tabNavigationMenuIn );
+			$menuItem.on( 'focusout', tabNavigationMenuOut );
+		} );
 
 		$navMenu.find( '.expand' ).on( 'click', expand );
 
