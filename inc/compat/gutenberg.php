a5b5a232d1b25d293e545bf7866b51c19b4f3b57
Rich Tabor <hi@richtabor.com>
2019-04-23 13:08:43 -0400

    Update primer_gutenberg_editor_assets() with revised asset loading

--- inc/compat/gutenberg.php
+++ inc/compat/gutenberg.php
@@ -16,29 +16,22 @@
  */
 function primer_gutenberg_theme_support() {
 
-	// Load regular editor styles into the new block-based editor.
-	// add_theme_support( 'editor-styles' );
-
 	// Load default block styles.
 	add_theme_support( 'wp-block-styles' );
 
 	// Add support for full and wide alignment.
 	add_theme_support( 'align-wide' );
 
+	// Enqueue editor styles if the block editor exists.
+	if ( function_exists( 'register_block_type' ) ) {
+
+		$suffix = SCRIPT_DEBUG ? '' : '.min';
+
+		add_theme_support( 'editor-styles' );
+
+		// Load regular editor styles into the new block-based editor.
+		add_editor_style( "assets/css/admin/editor-blocks{$suffix}.css" );
+	}
+
 }
 add_action( 'after_setup_theme', 'primer_gutenberg_theme_support' );
-
-/**
- * Enqueue styles for Gutenberg editor.
- *
- * @since 1.8.5
- */
-function primer_gutenberg_editor_assets() {
-
-	global $post;
-
-	$suffix = SCRIPT_DEBUG ? '' : '.min';
-
-	wp_enqueue_style( 'primer-block-editor-style', get_theme_file_uri( "assets/css/admin/editor-blocks{$suffix}.css" ), true, defined( 'PRIMER_CHILD_VERSION' ) ? PRIMER_CHILD_VERSION : PRIMER_VERSION, 'all' );
-}
-add_action( 'enqueue_block_editor_assets', 'primer_gutenberg_editor_assets' );
