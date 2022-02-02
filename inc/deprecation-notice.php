072cd1c20b4a2926edf12c8c0847cf58ee580e47
Evan Herman <evan.m.herman@gmail.com>
2022-02-02 15:11:11 -0500

    Add a deprecation notice to Primer

---
+++ inc/deprecation-notice.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Customizer bootstrap.
+ *
+ * @class    Primer_Deprecation_Notice
+ * @package  Classes/Deprecation_Notice
+ * @category Class
+ * @author   GoDaddy
+ * @since    NEXT
+ */
+class Primer_Deprecation_Notice {
+
+	/**
+	 * Class constructor.
+	 */
+	public function __construct() {
+
+		if ( get_theme_mod( 'deprecation_notice_dismissed', false ) ) {
+
+			return;
+
+		}
+
+		add_action( 'admin_notices', array( $this, 'deprecation_notice' ) );
+
+		add_action( 'wp_ajax_my_action', array( $this, 'my_action' ) );
+
+	}
+
+	/**
+	 * Show a dismissable admin notice about theme deprecation.
+	 */
+	public function deprecation_notice() {
+
+		$class   = 'notice notice-warning is-dismissible primer-deprecation-notice';
+		$message = sprintf(
+			/* translators: 1: test */
+			__( 'This theme will no longer receive support at the end of 2022. It is replaced by the free %1$s.', 'primer' ),
+			sprintf(
+				'<a href="https://wordpress.org/themes/go/" target="_blank" title="%1$s | WordPress.org">%1$s</a>',
+				esc_html__( 'Go Theme', 'primer' )
+			)
+		);
+
+		$this->print_dismissable_script();
+
+		printf(
+			'<div class="%1$s">
+				<p>%2$s</p>
+			</div>',
+			esc_attr( $class ),
+			wp_kses_post( $message )
+		);
+
+	}
+
+	/**
+	 * Update the theme mod to dismiss the deprecation notice.
+	 */
+	public function my_action() {
+
+		set_theme_mod( 'deprecation_notice_dismissed', true );
+
+		wp_die(); // this is required to terminate immediately and return a proper response
+
+	}
+
+	/**
+	 * Print the dismissable deprecation notice script.
+	 */
+	public function print_dismissable_script() {
+
+		?>
+		<script type="text/javascript">
+		/**
+		 * Admin code for dismissing notifications.
+		 *
+		 */
+		(function( $ ) {
+			'use strict';
+			$( function() {
+				$( '.primer-deprecation-notice' ).on( 'click', '.notice-dismiss', function( event, el ) {
+					jQuery.post(
+						ajaxurl,
+						{
+							'action': 'my_action',
+							'whatever': 1234
+						}
+					);
+				});
+			} );
+		})( jQuery );
+		</script>
+		<?php
+
+	}
+
+}
+
+new Primer_Deprecation_Notice();
